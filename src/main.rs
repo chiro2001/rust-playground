223f1b3fea3e1ff51d6d4ba695ecb92fcdf22227
Chiro <Chiro2001@163.com>
2022-05-06 11:57:43 +0800

    use 2021

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ fn greet_world() {
     let chinese = "世界，你好";
     let english = "World, hello";
     let regions = [southern_germany, chinese, english];
-    for region in regions.iter() {
+    for region in regions {
         println!("{}", &region);
     }
 }
