53ef690008d0c9b0df8279baea2b446ad9b3fc61
Chiro <Chiro2001@163.com>
2022-05-07 11:00:32 +0800

    read file content

--- src/bin/read-write-file.rs
+++ src/bin/read-write-file.rs
@@ -1,9 +1,20 @@
 #[warn(unused_variables)]
 use std::error::Error;
+use std::io;
+use std::fs;
 use std::{fs::File, io::Write};
+
+fn get_file_content(filename: &String) -> Result<String, io::Error> {
+	fs::read_to_string(filename)
+}
 fn main() -> Result<(), Box<dyn Error>> {
     let filename = String::from("test-file.tmp");
-    let res = File::create(&filename)?.write("Hello, World!".as_bytes())?;
+	let content = get_file_content(&filename);
+	let content = match content {
+		Ok(content) => content,
+		Err(_) => String::from("[Empty]")
+	};
+    let res = File::create(&filename)?.write((content + "\nHello, World!").as_bytes())?;
     print!("wrote {res} bytes to file {filename}");
     Ok(())
 }
