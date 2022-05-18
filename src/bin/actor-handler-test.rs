b1d51530ccc392d4f1fb9b35820508e9f5ff1889
Chiro <Chiro2001@163.com>
2022-05-18 17:08:11 +0800

    actor handler test 2

--- src/bin/actor-handler-test.rs
+++ src/bin/actor-handler-test.rs
@@ -4,7 +4,21 @@ use actix::prelude::*;
 // we have to define the response type (rtype)
 #[derive(Message)]
 #[rtype(result = "usize")]
-struct Sum(usize, usize);
+struct Data(usize, usize);
+
+#[derive(Message)]
+#[rtype(result = "usize")]
+struct Mul(usize, usize);
+
+trait GetData<T> {
+    fn calc(&self) -> T;
+}
+
+impl GetData<usize> for Data {
+    fn calc(&self) -> usize {
+        self.0 + self.1
+    }
+}
 
 // Actor definition
 struct Calculator;
@@ -14,21 +28,31 @@ impl Actor for Calculator {
 }
 
 // now we need to implement `Handler` on `Calculator` for the `Sum` message.
-impl Handler<Sum> for Calculator {
+impl Handler<Data> for Calculator {
     type Result = usize; // <- Message response type
 
-    fn handle(&mut self, msg: Sum, _ctx: &mut Context<Self>) -> Self::Result {
+    fn handle(&mut self, msg: Data, _ctx: &mut Context<Self>) -> Self::Result {
         msg.0 + msg.1
+    }
+}
+
+impl Handler<Mul> for Calculator {
+    type Result = usize; // <- Message response type
+
+    fn handle(&mut self, msg: Mul, _ctx: &mut Context<Self>) -> Self::Result {
+        msg.0 * msg.1
     }
 }
 
 #[actix::main] // <- starts the system and block until future resolves
 async fn main() {
-    let addr = Calculator.start();
-    let res = addr.send(Sum(10, 5)).await; // <- send message and get future for result
-
-    match res {
-        Ok(result) => println!("SUM: {}", result),
-        _ => println!("Communication to the actor has failed"),
+    // let data_to_send = vec![Data(114, 514), Mul(3, 4)];
+    match Calculator.start().send(Data(114, 514)).await {
+        Ok(result) => println!("add result is: {result}"),
+        _ => println!("ERR")
+    }
+    match Calculator.start().send(Mul(114, 514)).await {
+        Ok(result) => println!("add result is: {result}"),
+        _ => println!("ERR")
     }
 }
